800b43d8c031f15fd68834cc9b8160a76985c9fa
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2024-04-16 01:20:49 +0200

    16/04/2024

--- car-rest-service/src/main/java/ua/com/foxminded/yuriy/carrestservice/controller/BrandController.java
+++ car-rest-service/src/main/java/ua/com/foxminded/yuriy/carrestservice/controller/BrandController.java
@@ -1,8 +1,26 @@
 package ua.com.foxminded.yuriy.carrestservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BrandController {
+import lombok.AllArgsConstructor;
+import ua.com.foxminded.yuriy.carrestservice.entities.Brand;
+import ua.com.foxminded.yuriy.carrestservice.service.BrandService;
 
+@RestController
+@RequestMapping("/api/v1/brands")
+@AllArgsConstructor
+public class BrandController {
+	
+	private final BrandService brandService;
+	
+	@PostMapping
+   public ResponseEntity<Brand> createBrand(@RequestBody Brand brand) {
+       Brand createdBrand = brandService.save(brand);
+       return ResponseEntity.status(HttpStatus.CREATED).body(createdBrand);
+   }
 }
